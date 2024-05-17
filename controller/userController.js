fa33223ff66088e3e5af46088c74a1f69a7d120e
natan-teklie <natanmitiku232@gmail.com>
2024-05-17 18:51:04 +0300

    some changes updated

--- controller/userController.js
+++ controller/userController.js
@@ -1,67 +1,90 @@
 //db
-const express = require ('express')
-const dbConnection = require('../db/dbConfig')
-const bcrypt = require ('bcrypt')
-const {StatusCodes} = require('http-status-codes')
+const express = require("express");
+const dbConnection = require("../db/dbConfig");
+const bcrypt = require("bcrypt");
+const { StatusCodes } = require("http-status-codes");
+const jwt = require('jsonwebtoken')
 
+async function Register(req, res) {
+  const { username, firstname, lastname, email, password } = req.body;
 
-
-async function Register(req, res){
-    const {username, firstname, lastname, email, password} = req.body;
-
-    if(!username || !firstname || !lastname|| !email || !password){
-       return res.status(StatusCodes.BAD_REQUEST).json({msg:"please enter all required fields"})
-    }
-    try {
-        const [user] = await dbConnection.query("select username, userid from users where username = ? or email = ?",[username,email])
-        ////Elias codes optional
+  if (!username || !firstname || !lastname || !email || !password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please enter all required fields" });
+  }
+  try {
+    const [user] = await dbConnection.query(
+      "select username, userid from users where username = ? or email = ?",
+      [username, email]
+    );
+    ////Elias codes optional
     //    const user = await dbConnection.query("select * from users where username = ? or email = ?",[username, email])
     //    if(user){
     //     return res.status(400).json({msg:"user already existed"})
     //    }
-        if(user.length > 0){
-            return res.status(StatusCodes.BAD_REQUEST).json({msg:"user already registered"})
-        }
-        if(password.length < 8){
-            return res.status(StatusCodes.BAD_REQUEST).json({msg:"password must be at least 8 character"})
-        }
-
-//password encryption
-const salt = await bcrypt.genSalt()
-const hashedPassword = await bcrypt.hash(password, salt)
-
-        await dbConnection.query("INSERT INTO users (username, firstname, lastname, email, password) VALUES(?,?,?,?,?)", [username, firstname, lastname, email, hashedPassword])
-        return res.status(StatusCodes.CREATED).json({msg:"user created"})
-    } catch (error) {
-        console.log(error.message)
-       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"something went wrong try again later"})
+    if (user.length > 0) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "user already registered" });
     }
-}
-
-
-
-
-
-
-
-
-async function Login(req, res){
-    const {email, password} = req.body;
-    if(!email || !password ){
-        return res.status(StatusCodes.BAD_REQUEST).json({msg:"please fill all required fields"})
+    if (password.length < 8) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "password must be at least 8 character" });
     }
 
-    try {
-        const [user] = await dbConnection.query("select username, userid from users where email = ?", [email])
-      
+    //password encryption
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-    } catch (error) {
-        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:"something went wrong please try again"})
-    }
+    await dbConnection.query(
+      "INSERT INTO users (username, firstname, lastname, email, password) VALUES(?,?,?,?,?)",
+      [username, firstname, lastname, email, hashedPassword]
+    );
+    return res.status(StatusCodes.CREATED).json({ msg: "user created" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "something went wrong try again later" });
+  }
 }
-function Check(req, res){
-    res.send ("Well Come! We check users authentication")
+
+async function Login(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "please fill all required fields" });
+  }
+
+  try {
+    const [user] = await dbConnection.query(
+      "select username, userid from users where email = ?",
+      [email]
+    );
+    // return res
+    //   .status(StatusCodes.ACCEPTED)
+    //   .json({ msg: "hello you are loged in" });
+
+    //JSON WEB TOKEN
+
+
+    const username = user[0].username;
+    const userid = user[0].userid;
+    
+  const token = jwt.sign({username, userid}, 'secrte', {expiresIn: '1d'})
+  return res.status(StatusCodes.OK).json({msg: "successfuly loged in", token})
+
+  } catch (error) {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "something went wrong please try again" });
+  }
+}
+function Check(req, res) {
+  res.send("Well Come! We check users authentication");
 }
 
-
-module.exports = {Register, Login, Check}
+module.exports = { Register, Login, Check };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-
+//controller file
 const {Register, Login, Check} = require('../controller/userController')
 
 
